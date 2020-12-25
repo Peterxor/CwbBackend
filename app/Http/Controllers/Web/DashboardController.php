c3ebb6d5482a51b656541468754fcde5059bde0c
elaine <elaine@larvata.tw>
2020-12-25 19:17:02 +0800

    fix

--- app/Http/Controllers/Web/DashboardController.php
+++ app/Http/Controllers/Web/DashboardController.php
@@ -8,7 +8,7 @@ class DashboardController extends Controller
 {
     public function index()
     {
-        return view("backend.pages.dashboard.edit");
+        return view("backend.pages.dashboard.index");
     }
 
     public function query()
