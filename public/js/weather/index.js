9be5dbd590d86e1dbc7c1a933d668bd931f18acd
elaine <elaine@larvata.tw>
2020-12-27 02:41:17 +0800

    fix: weather

--- public/js/weather/index.js
+++ public/js/weather/index.js
@@ -21,6 +21,8 @@ $(document).ready(function() {
         destroy: true,
         autoWidth: true,
         ordering: false,
+        paging: false,
+        info: false,
         language: datatable_lang_tw,
         ajax: {
             url: categoryUrl,
@@ -46,7 +48,7 @@ $(document).ready(function() {
         },
         columns: [{
                 data: null,
-                width: '250px',
+                width: '40px',
                 defaultContent: '',
                 mRender: function(data, type, full) {
                     return '<tr><th><a href="" class="btn btn-outline-secondary btn-sm"><i class="la la-sort" style="font-size:16px"></i></a></th>';
@@ -54,7 +56,7 @@ $(document).ready(function() {
             },
             {
                 data: null,
-                width: '250px',
+                width: '300px',
                 defaultContent: '',
                 mRender: function(data, type, full) {
                     return '<td><input class="form-control" type="text" value="' + data.name + '" name="name[]" id="name-input" required\
@@ -64,13 +66,13 @@ $(document).ready(function() {
             {
                 data: null,
                 width: '250px',
+                className: 'text-center',
                 defaultContent: '',
                 mRender: function(data, type, full) {
-                    return '<td><i class="category-trash la la-trash" data-category-id="' + data.id + '" style="font-size:16px"></i></td>\
+                    return '<td><a href="" class="btn btn-outline-secondary btn-sm"><i class="category-trash la la-trash" data-category-id="' + data.id + '" style="font-size:16px"></i></a></td>\
                                     </tr>';
                 }
             },
-
         ],
         rowReorder: {
             dataSrc: 'sequence'
@@ -200,7 +202,7 @@ $(document).ready(function() {
                                         <th><a href="" class="btn btn-outline-secondary btn-sm"><i class="la la-sort" style="font-size:16px"></i></a></th>\
                                         <td><input class="form-control" type="text" value="" name="name[]" id="name-input" required\
                                        maxlength="30"></td>\
-                                        <td><a href="" class="btn btn-outline-secondary btn-sm"><i class="la la-trash" style="font-size:16px"></i></a></td>\
+                                        <td><a href="" class="btn btn-outline-secondary btn-sm text-center"><i class="la la-trash" style="font-size:16px"></i></a></td>\
                                     </tr>');
     });
 
