4a9d0de022019f1429eddb022bbb34fdeab13477
elaine <elaine@larvata.tw>
2020-12-27 01:59:11 +0800

    feat: 事件紀錄

---
+++ public/js/active/index.js
@@ -0,0 +1,81 @@
+$(document).ready(function() {
+    // 時間設定
+    $("#start-date").datepicker({
+        autoclose: true,
+        language: 'zh-TW',
+        format: "yyyy-mm-dd",
+    }).on('changeDate', function() {
+        // console.log('yes');
+        if ($("#start-date").val() > $('#end-date').val()) {
+            $('#end-date').val('');
+        }
+        $('#end-date').datepicker('setStartDate', $("#start-date").val());
+        $('#end-date').focus();
+    });
+    $("#end-date").datepicker({
+        autoclose: true,
+        language: 'zh-TW',
+        format: "yyyy-mm-dd",
+    });
+
+    var searchForm = $('#search-form');
+    var searchUrl = searchForm.attr('action');
+    var activeTable = $('#active-table');
+    var _activeTable;
+    var editUrl = activeTable.attr('data-edit-url');
+
+    $.fn.dataTable.ext.errMode = 'none';
+    var dataTableSettings = {
+        stateSave: false,
+        processing: true,
+        serverSide: true,
+        searching: false,
+        destroy: true,
+        autoWidth: true,
+        ordering: false,
+        language: datatable_lang_tw,
+        ajax: {
+            url: searchUrl,
+            type: 'GET',
+            data: function(aoData) {
+                //把分頁的參數與自訂的搜尋結合
+                $.each(searchForm.serializeArray(), function(key, value) {
+                    aoData[value.name] = value.value;
+                });
+                return aoData;
+            },
+            dataSrc: 'data',
+            error: function(xhr, error, code) {
+                // console.log('xhr',xhr);
+                // console.log('code',code);
+                // console.log('error',error);
+                if (code == 'Forbidden') {
+                    location.reload();
+                } else if (xhr.status == 419) {
+                    location.reload();
+                }
+            }
+        },
+        columns: [{
+            data: 'date',
+            width: '100px',
+        }, {
+            data: 'user',
+            width: '150px',
+        }, {
+            data: 'active',
+            width: '150px',
+        }, {
+            data: 'item',
+            width: '150px',
+        }, {
+            data: 'ip',
+            width: '150px',
+        }],
+        "rowCallback": function(row, data, index) {
+
+        },
+    }
+
+    _activeTable = activeTable.DataTable(dataTableSettings);
+});
---
+++ resources/views/backend/pages/active/index.blade.php
@@ -0,0 +1,84 @@
+@extends('backend.layouts.app')
+@section('content')
+    <!-- begin:: Content Head -->
+<div class="kt-subheader  kt-grid__item" id="kt_subheader">
+    <div class="kt-container  kt-container--fluid ">
+        <div class="kt-subheader__main">
+            <h3 class="kt-subheader__title">事件紀錄</h3>
+            <span class="kt-subheader__separator kt-subheader__separator--v"></span>
+        </div>
+
+    </div>
+</div>
+<!-- end:: Content Head -->
+<!-- begin:: Content -->
+<div class="kt-container  kt-container--fluid  kt-grid__item kt-grid__item--fluid">
+
+    <div class="row">
+        <div class="kt-portlet">
+        <form class="kt-form kt-form--label-right" id="search-form" action="{{env('URL_PREFIX','').route('active.query',[],false)}}">
+            <input type="hidden" name="role">
+            <div class="kt-portlet__body">
+                <div class="form-group row">
+                    <div class="col-lg-4">
+                        <label>操作行為:</label>
+                        <select name="active_type" class="form-control">
+                            <option value="all">所有行為</option>
+                            <option value="login">登入</option>
+                            <option value="logout">登出</option>
+                            <option value="add">新增</option>
+                            <option value="edit">修改</option>
+                            <option value="delete">刪除</option>
+                        </select>
+                    </div>
+                    <div class="col-lg-6">
+                        <label>日期</label>
+                        <div class="input-daterange input-group" id="">
+                            <input type="text" class="form-control" name="start_date" id="start-date" value="">
+                            <div class="input-group-append">
+                                <span class="input-group-text"><i class="la la-ellipsis-h"></i></span>
+                            </div>
+                            <input type="text" class="form-control" name="end_date" id="end-date" value="">
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="kt-portlet__foot">
+              <div class="kt-form__actions">
+                <div class="row">
+                  <div class="col-lg-12 kt-align-right">
+                    <button type="button" class="btn btn-secondary" id="search-btn">搜尋</button>
+                  </div>
+                </div>
+              </div>
+            </div>
+        </form>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="kt-portlet">
+            <div class="kt-portlet__body">
+                <table id="active-table" class="table table-striped- table-bordered table-hover table-checkable dataTable no-footer dtr-inline">
+                    <thead>
+                        <tr>
+                            <th>日期時間</th>
+                            <th>操作使用者</th>
+                            <th>操作行為</th>
+                            <th>項目</th>
+                            <th>Ip Address</th>
+                        </tr>
+                    </thead>
+                </table>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- end:: Content -->
+
+@endsection
+
+@section('pages_scripts')
+    {!! Html::script('vendor/audit/bootstrap-datepicker.min.js') !!}
+    {!! Html::script(env('URL_PREFIX','').'js/active/index.js') !!}
+@endsection
