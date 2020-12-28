77882b8db81019178ba6b421ba8eb96bf99a0ed5
set <peter55605@gmail.com>
2020-12-28 14:39:38 +0800

    dashboard save anchor

--- public/js/dashboard/index.js
+++ public/js/dashboard/index.js
@@ -5,4 +5,25 @@ $(document).ready(function() {
         $(this).parent().parent().find('button[name=save_btn]').show();
         $(this).hide();
     })
+
+    $('.js-submit-btn').on('click', function(e) {
+        var device_id = e.target.getAttribute('data-device-id')
+        console.log(device_id)
+        var host_id = document.getElementById('device-host-' + device_id).value
+        console.log(host_id)
+        $.ajax({
+            url: '/device/updateDeviceHost',
+            type: 'PUT',
+            data: {
+                user_id: host_id,
+                device_id,
+            },
+            success: function(data) {
+                if (data.success) {
+                    swal.fire('success')
+                }
+            }
+        });
+
+    })
 });
