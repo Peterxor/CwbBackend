cc5c9ab9fad18d28cde542de9a6b4e9496142612
set <peter55605@gmail.com>
2021-01-27 15:09:43 +0800

    更改圖塊格式

--- config/weatherlayout.php
+++ config/weatherlayout.php
@@ -2,7 +2,7 @@
 
 return array(
     ['name' => 'weather-information', 'display_name' => '一般天氣預報', 'children' => [
-        ['name' => 'block', 'display_name' => '圖卡區塊', 'class_name' => 'blcok_type_1', 'type' => '1'],
+        ['name' => 'block', 'display_name' => '圖卡區塊', 'class_name' => 'blcok_type_1', 'type' => '2'],
     ]], ['name' => 'general', 'display_name' => '通用設定', 'children' => [
         ['name' => 'tool-left', 'display_name' => '工具列 (左)', 'class_name' => 'blcok_type_2', 'type' => '2'],
         ['name' => 'tool-right', 'display_name' => '工具列 (右)', 'class_name' => 'blcok_type_2', 'type' => '2'],
