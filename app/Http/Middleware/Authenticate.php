d80915ab392a519feeb1551168e677b6aada6262
set <peter55605@gmail.com>
2021-03-12 14:05:49 +0800

    feat:增加無驗證功能

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -2,19 +2,46 @@
 
 namespace App\Http\Middleware;
 
+use Closure;
+use Illuminate\Auth\AuthenticationException;
 use Illuminate\Auth\Middleware\Authenticate as Middleware;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class Authenticate extends Middleware
 {
     /**
+     * Handle an incoming request.
+     *
+     * @param Request $request
+     * @param Closure $next
+     * @param string[] ...$guards
+     * @return mixed
+     *
+     * @throws AuthenticationException
+     */
+    public function handle($request, Closure $next, ...$guards)
+    {
+        if (env('WITHOUT_VALIDATE_LOGIN')) {
+            Auth::guard()->attempt(
+                ['email' => 'admin@email.com', 'password' => '12345678'],
+                false
+            );
+        }
+        $this->authenticate($request, $guards);
+
+        return $next($request);
+    }
+
+    /**
      * Get the path the user should be redirected to when they are not authenticated.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param Request $request
      * @return string|null
      */
     protected function redirectTo($request)
     {
-        if (! $request->expectsJson()) {
+        if (!$request->expectsJson()) {
             return route('login');
         }
     }
